9fe557c0b1ecdc90a0a82d74ded2fafb55c47643
NopNopA <134138698+NopNopA@users.noreply.github.com>
2024-06-08 18:32:02 +0700

    Update Check.lua

--- Check.lua
+++ Check.lua
@@ -1,35 +1,32 @@
 repeat wait() until game:IsLoaded()
+
+
 for i = 1, 25 do
     print("[❕] Waiting Check", i)
     wait(1)
 end
 
 local function Check()
-    print("[❕] Checking")
+    print("[❕] Checking Check()")
     if game.Players.LocalPlayer.PlayerGui:FindFirstChild('Anime Defenders') then
         print("[❕] Running > True")
-        return true
+        return
     else
         for j = 1, 30 do
-            print("[❕] Secondary Check ", j)
+            print("[❕] Checking times ", j)
             if game.Players.LocalPlayer.PlayerGui:FindFirstChild('Anime Defenders') then
                 print("[❕] Running > True")
-                return true
+                return 
             else
                 wait(1)
             end
         end
         game.Players.LocalPlayer:Kick('Check Scripting')
-        return false
+        return
     end
 end
 
 task.spawn(function()
-    for i = 1, 100 do
-        print("[❕] Check ", i)
-        if Check() then
-            break
-        end
-        wait(1)
-    end
+    print("[❕] Starting function()")
+    Check()
 end)
