65094182c4943ea3640c6c5658a7059dcdbaf3f8
NopNopA <134138698+NopNopA@users.noreply.github.com>
2024-12-27 20:50:05 +0700

    Create Robloxs.lua

---
+++ Robloxs.lua
@@ -0,0 +1,70 @@
+repeat task.wait() until game:IsLoaded()
+local HttpService = game:GetService("HttpService")
+local Request = (syn and syn.request) or request or (http and http.request) or http_request
+local username = game.Players.LocalPlayer.Name
+local globalFunc = false
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = 'API SERVICES',
+    Text = 'User : ' .. username,
+    Duration = 25,
+    Icon = 'rbxassetid://18976336309'
+})
+
+local function checkForError()
+    game.CoreGui.RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(v)
+        if v.Name == "ErrorPrompt" then
+            pcall(function()
+                repeat wait(0.5) until game.CoreGui.RobloxPromptGui.promptOverlay.ErrorPrompt.MessageArea.ErrorFrame:FindFirstChild("ErrorMessage")
+                
+                local errorMessage = game.CoreGui.RobloxPromptGui.promptOverlay.ErrorPrompt.MessageArea.ErrorFrame.ErrorMessage.Text
+                local errorCode = tonumber(errorMessage:split("\n")[2]:match("%d+"))
+
+                if errorCode ~= 772 and errorCode ~= 773 then
+                    globalFunc = true
+                end
+            end)
+        end
+    end)
+    return globalFunc
+end
+
+local function updateStatus()
+    local url = "http://127.0.0.1:1471/"
+    local username = game.Players.LocalPlayer.Name
+    local data = "username=" .. username
+
+    local requestData = {
+        Url = url,
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/x-www-form-urlencoded"
+        },
+        Body = data
+    }
+
+    local success, response = pcall(function()
+        return Request(requestData)
+    end)
+
+    if success and response.StatusCode == 200 then
+        print("Status Updated API : " .. username)
+    else
+        warn("Failed Update API " .. (response and response.StatusCode or "Request failed"))
+    end
+end
+
+local Round = 0
+wait(2)
+
+while true do
+    globalFunc = checkForError()
+    print("Disconnected : ", globalFunc)
+    print("API runs at : ", Round)
+    if not globalFunc then
+        updateStatus()
+    end
+    Round = Round + 1
+    print("----------------------------------------")
+    wait(20)
+end
